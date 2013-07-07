e8bafbae560a6e9f4704b6f499d1eb543ea53ef2
vadik777qwe <vadik@vadik-pc>
2013-07-07 08:07:26 +0400

    New random algorithm

--- src/AgressKey/keyRandom.java
+++ src/AgressKey/keyRandom.java
@@ -1,11 +1,9 @@
 package AgressKey;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class keyRandom {
-	public static ArrayList b = new ArrayList();
-	
+	public static String CharRandom = "qwertyuiopasdfghjklzxcvbnm";
 	
 	public static String key() {
 		return random()+number()+random()+number()+"-"+number()+number()+random()+random()+"-"+random()+random()+random()+random();
@@ -18,34 +16,9 @@ public class keyRandom {
 
 	
 	public static String random() {
-		b.add("q");
-		b.add("w");
-		b.add("e");
-		b.add("r");
-		b.add("y");
-		b.add("u");
-		b.add("i");
-		b.add("o");
-		b.add("p");
-		b.add("a");
-		b.add("s");
-		b.add("d");
-		b.add("f");
-		b.add("g");
-		b.add("h");
-		b.add("j");
-		b.add("k");
-		b.add("l");
-		b.add("z");
-		b.add("x");
-		b.add("c");
-		b.add("v");
-		b.add("b");
-		b.add("n");
-		b.add("m");
 		Random random = new Random();
+		return String.valueOf(CharRandom.charAt(random.nextInt(CharRandom.length())));
 
-		return (String) b.get(random.nextInt(25));
 		
 	}
 }
